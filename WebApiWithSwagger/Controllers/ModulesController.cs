068e77c7bea27fc6911c44fe0ad39c00cab817ea
Prashanth Mekala <c.prashanth@shipbob.com>
2018-12-13 18:16:17 +0530

    test

--- WebApiWithSwagger/Controllers/ModulesController.cs
+++ WebApiWithSwagger/Controllers/ModulesController.cs
@@ -25,7 +25,6 @@ namespace WebApiWithSwagger.Controllers
                 scriptInfo = service.RetrieveScriptDetailsDataByWhereData(data);//id is ScriptId
                 if (scriptInfo == null)
                 {
-
                     return null;
                 }
                 return Ok(scriptInfo);
